b27a9c719b5c600a9c034c4f5e6159b3d7b04b38
ZhenyuHe <849416763@qq.com>
2018-10-30 23:07:53 +0800

    修改 Window 编译条件

--- threadPool/ThreadPoolFactory.cpp
+++ threadPool/ThreadPoolFactory.cpp
@@ -1,6 +1,6 @@
 #include "../include/ThreadPoolFactory.h"
 
-#if defined(WIN32)
+#if defined(_WIN32) || defined(_WIN64)
     #include "window/WinThreadPool.h"
 #elif defined(linux) || defined(__APPLE__)
     #include "linux/LinuxThreadPool.h"
@@ -15,7 +15,7 @@ ThreadPoolFactory* ThreadPoolFactory::GetInstance() {
 }
 
 ThreadPool* ThreadPoolFactory::GetThreadPool() {
-#if defined(WIN32)
+#if defined(_WIN32) || defined(_WIN64)
 	return new WinThreadPool();
 #elif defined(linux) || defined(__APPLE__)
     return new LinuxThreadPool();
